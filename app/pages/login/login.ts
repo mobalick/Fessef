1aeb809db03c4df4a5e504babe9ff055e4b1db17
Malick <mobalick@hotmail.com>
2016-07-12 18:41:13 +0200

    Annonce detail page + nicer menu

--- app/pages/login/login.ts
+++ app/pages/login/login.ts
@@ -17,114 +17,114 @@ import {FIREBASE_PROVIDERS, defaultFirebase,AngularFire, FirebaseAuth } from 'an
  })
 
 export class LoginPage {
-  public authType;
-  public isAuthorized;
-  public loginForm;
-  public signupForm;
-  public user : User;
-  public submitAttempt;
-  public isLoginIncorrect;
-  public storage = new Storage(LocalStorage);
+    public authType;
+    public isAuthorized;
+    public loginForm;
+    public signupForm;
+    public user : User;
+    public submitAttempt;
+    public isLoginIncorrect;
+    public storage = new Storage(LocalStorage);
 
-  constructor(public nav: NavController, public userService : UserService, 
-              public formBuilder : FormBuilder, public notif : NotificationService,
-              public auth : FirebaseAuth ) {
-    
-    this.authType         = 'login';  
-    this.isLoginIncorrect = false;
-    this.submitAttempt    =false;
-
-    this.user             = this.userService.user;
-    this.isAuthorized     =this.userService.IsAuthorized();
-    
-    this.loginForm        = formBuilder.group({
-      'email': ['', Validators.required],
-      'password': ['', Validators.required]
-    });
-
-     this.signupForm      = formBuilder.group({
-      'name'    : ['', Validators.required],
-      'lastName': ['', Validators.required],
-      'sex'     : ['m', Validators.required],
-      'email'   : ['', Validators.required],
-      'school'  : ['', Validators.required],
-      'city'    : ['', Validators.required],
-      'dob'     : ['', Validators.required],
-      'password': ['', Validators.required],
-    });
-    
-    
-  }
-
-  public login(login)
-  {
-    console.log("call from login");
-    
-    this.notif.showLoading(this.nav);
-    
-    this.userService.login(login).then((authData)=>{
-       this.notif.closeLoading();
-       
-      this.userService.get(authData.uid).subscribe(user =>{
-        this.userService.user             = user;
-        this.userService.user.isLogedIn   = true;
-        this.isAuthorized                 = true;
-        this.user                         = this.userService.user;
-
-        this.storage.setJson('user', user);
+    constructor(public nav: NavController, public userService : UserService, 
+                public formBuilder : FormBuilder, public notif : NotificationService,
+                public auth : FirebaseAuth ) {
         
-        this.nav.setRoot(HomePage);
-      });
+        this.authType         = 'login';  
+        this.isLoginIncorrect = false;
+        this.submitAttempt    = false;
+
+        this.user             = this.userService.user;
+        this.isAuthorized     = this.userService.IsAuthorized();
         
-    }).catch((error)=>{
-      this.notif.showError(error,this.nav);
-      this.isLoginIncorrect   = true;
-    })
-  }
-
-  public logout()
-  {
-
-    console.log("call from logout");
-    this.userService.logout();
-    this.isAuthorized=false;
-    this.authType = 'login'; 
-  }
-
-  public signup(user:User)
-  {
-    this.submitAttempt = true;
-
-    if (this.signupForm.valid) {
-      user.isLogedIn=false;
-
-      this.notif.showLoading(this.nav);
-
-      this.userService.create(user).then((authdata)=>{
-        console.log("create ok"+authdata);
-        this.notif.closeLoading();
-        this.authType = 'login';
-       
-       //Save the object
-       user.uid = authdata.uid;
-       this.userService.save(user);
-
-
-      }).catch((error)=>{
-        console.log(error);
-        this.notif.showError(error.message,this.nav);
-        this.authType = 'signup';
-      });
-      
-      
+        this.loginForm        = formBuilder.group({
+        'email': ['', Validators.required],
+        'password': ['', Validators.required]
+        });
+    
+        this.signupForm      = formBuilder.group({
+        'name'    : ['', Validators.required],
+        'lastName': ['', Validators.required],
+        'sex'     : ['m', Validators.required],
+        'email'   : ['', Validators.required],
+        'school'  : ['', Validators.required],
+        'city'    : ['', Validators.required],
+        'dob'     : ['', Validators.required],
+        'password': ['', Validators.required],
+        });
+        
+        
     }
-    else
+
+    public login(login)
     {
-      
-    }
-    
-    console.log(user.name);
+        console.log("call from login");
+        
+        this.notif.showLoading(this.nav);
+        
+        this.userService.login(login).then((authData)=>{
+        this.notif.closeLoading();
+        
+        this.userService.get(authData.uid).subscribe(user =>{
+            this.userService.user             = user;
+            this.userService.user.isLogedIn   = true;
+            this.isAuthorized                 = true;
+            this.user                         = this.userService.user;
 
-  }
+            this.storage.setJson('user', user);
+            
+            this.nav.setRoot(HomePage);
+        });
+            
+        }).catch((error)=>{
+        this.notif.showError(error,this.nav);
+        this.isLoginIncorrect   = true;
+        })
+    }
+
+    public logout()
+    {
+
+        console.log("call from logout");
+        this.userService.logout();
+        this.isAuthorized=false;
+        this.authType = 'login'; 
+    }
+
+    public signup(user:User)
+    {
+        this.submitAttempt = true;
+
+        if (this.signupForm.valid) {
+        user.isLogedIn=false;
+
+        this.notif.showLoading(this.nav);
+
+        this.userService.create(user).then((authdata)=>{
+            console.log("create ok"+authdata);
+            this.notif.closeLoading();
+            this.authType = 'login';
+        
+        //Save the object
+        user.uid = authdata.uid;
+        this.userService.save(user);
+
+
+        }).catch((error)=>{
+            console.log(error);
+            this.notif.showError(error.message,this.nav);
+            this.authType = 'signup';
+        });
+        
+        
+        }
+        else
+        {
+        
+        }
+        
+        console.log(user.name);
+
+    }
 
 }
